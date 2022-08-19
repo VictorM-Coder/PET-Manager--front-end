cf65fbec36748c27d6bb91626d0a2b23e16589a2
Victor Martins <martinsvictor6781@gmail.com>
2022-08-19 09:22:00 -0300

    criação do layout do formulário de cadastro

---
+++ PET-Manager/src/app/forms/create-button-bar/create-button-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-button-bar',
+  templateUrl: './create-button-bar.component.html',
+  styleUrls: ['./create-button-bar.component.css']
+})
+export class CreateButtonBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- PET-Manager/src/app/forms/forms.module.ts
+++ PET-Manager/src/app/forms/forms.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PetFormComponent } from './pet-form/pet-form.component';
+import { CreateButtonBarComponent } from './create-button-bar/create-button-bar.component';
 
 
 
 @NgModule({
   declarations: [
     PetFormComponent,
+    CreateButtonBarComponent,
   ],
   imports: [
     CommonModule

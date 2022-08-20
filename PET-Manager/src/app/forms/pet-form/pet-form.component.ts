c6a12a0fecf4738c38f1fd0268c682a488ccee4a
Victor Martins <martinsvictor6781@gmail.com>
2022-08-20 09:27:38 -0300

    inicio da implementação do two away data binding

--- PET-Manager/src/app/forms/pet-form/pet-form.component.ts
+++ PET-Manager/src/app/forms/pet-form/pet-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Pet } from './../../model/pet';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'pet-form',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pet-form.component.css']
 })
 export class PetFormComponent implements OnInit {
+
+  pet: Pet = {
+    id: 0,
+    name: '',
+    breed: '',
+    weight: 0,
+    birthday: new Date('1995-12-17T03:24:00'),
+    gender: '',
+    vaccinated: false,
+    animalClass: '',
+    image: null,
+  };
 
   constructor() { }
 
---
+++ PET-Manager/src/app/model/pet.ts
@@ -0,0 +1,11 @@
+export interface Pet{
+  id: number,
+  name: string,
+  breed: string,
+  weight: number,
+  birthday: Date,
+  gender: string,
+  vaccinated: boolean,
+  animalClass: string,
+  image: File | null,
+}

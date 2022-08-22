b20cd16727cc9cbd375aa2b89d8c97917af32b05
Victor Martins <martinsvictor6781@gmail.com>
2022-08-22 11:07:09 -0300

    inicialização da classe de services

--- PET-Manager/src/app/model/pet-default.ts
+++ PET-Manager/src/app/model/pet-default.ts
@@ -4,8 +4,8 @@ export let petDefault = {
   breed: '',
   weight: 0,
   birthday: new Date('1900-10-10T03:24:00'),
-  gender: 'MALE',
+  gender: '',
   vaccinated: false,
-  animalClass: 'MAMMAL',
+  animalClass: '',
   image: null,
 }
